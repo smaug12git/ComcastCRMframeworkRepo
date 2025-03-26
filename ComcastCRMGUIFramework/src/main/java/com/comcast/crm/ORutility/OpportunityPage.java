f6e72a99c259f601770e6690648f6fec55118f2e
SOMU <SOMU@DESKTOP-MMSR92P>
2025-03-26 12:18:02 +0530

    oppertunity page have been modified

--- ComcastCRMGUIFramework/src/main/java/com/comcast/crm/ORutility/OpportunityPage.java
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/ORutility/OpportunityPage.java
@@ -4,11 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.Test;
+
 
 public class OpportunityPage {
 		WebDriver driver;
-	@Test
+	
 	public OpportunityPage(WebDriver driver)
 	{
 		this.driver=driver;
